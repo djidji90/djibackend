f9f78fe0263b68be14d72e0f61803f018d570c58
djidji90 <machimboleoncio@gmail.com>
2026-02-06 20:12:16 +0100

    update requirements for production

--- backend/settings.py
+++ backend/settings.py
@@ -100,6 +100,11 @@ if DEBUG:
     CSRF_TRUSTED_ORIGINS.extend([
         "http://localhost:8000",
         "http://127.0.0.1:8000",
+        "http://localhost:5173" ,
+        "http://localhost:5174" ,
+        "http://localhost:5176 ",
+
+
     ])
 
 CORS_ALLOW_CREDENTIALS = True
