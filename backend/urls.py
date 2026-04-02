05229eacbf372cc3b28a99c8a35f9db05e39e9db
djidji90 <machimboleoncio@gmail.com>
2026-04-02 19:18:35 +0100

    mejorado y recuperado el superuser

--- backend/urls.py
+++ backend/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('musica/', include('musica.urls')),
     path('api2/', include('api2.urls')),
-    # path('wallet/', include('wallet.urls')),
-   #  path('notifications/', include('notifications.urls')),
+    path('wallet/', include('wallet.urls')),
+    path('notifications/', include('notifications.urls')),
     # 👉 raíz personalizada que lista todos los endpoints
     path('', api_home, name='api-home'),
      path('artist/dashboard/', include('artist_dashboard.urls')),
